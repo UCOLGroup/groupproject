4ca56bf096ebcb73c595c741d5c95ee6673f02a9
Neil Asplin <9902562@uict.nz>
2016-10-13 11:23:36 +1300

    lecturer page commented

--- WebBased/WebBased/Lecturer.aspx.cs
+++ WebBased/WebBased/Lecturer.aspx.cs
@@ -31,21 +31,28 @@ namespace WebBased
                 // Create a new OLE Command object.  THis will store a Select statement
                 OleDbCommand command = new OleDbCommand();
 
+                // Adding the connection object to the command object
                 command.Connection = connection;
 
+                // Adding the select statement that will select the lecturer with the id held in the session id
                 command.CommandText = "Select * FROM lecturers WHERE user_name = '" + Session["USER_ID"].ToString() + "'"; //
 
+                // Create a OLE reader object from the command ExecuteReader method.
                 OleDbDataReader reader = command.ExecuteReader();
 
+                // When the reader obect uses the Read() method, it will retreive one record from the table.
                 reader.Read();
 
+                // Add the lecturer id into the text box using the reader object.
                 TextBox1.Text = reader["lecturer_id"].ToString();
 
+                // If the text only says Welcome, then the username needs to  
                 if (lblWelcome.Text == "Welcome")
                 {
                     lblWelcome.Text += " " + reader["first_name"].ToString() + " " + reader["last_name"].ToString();
                 }
 
+                // If the user is an admin change the gridviews to show all papers otherwise, limit papers to what the lecturer is teaching.
                 if (TextBox1.Text == "1")
                 {
                     GridView1.Visible = false;
@@ -59,17 +66,21 @@ namespace WebBased
                 }
 
             }
+
+            // If the session id is empty, redirect user to the login page 
             else
             {
                 Response.Redirect("User_Login_Page.aspx");
             }
         
 
-
                 if (!this.IsPostBack)
             {
+
+                //This area is used for testing 
+
                 //Attribute to show the Plus Minus Button.
-                GridView1.HeaderRow.Cells[0].Attributes["data-class"] = "expand";
+                //GridView1.HeaderRow.Cells[0].Attributes["data-class"] = "expand";
 
                 //Attribute to hide column in phone.
 
@@ -89,8 +100,11 @@ namespace WebBased
 
         protected void btnLogout_Click(object sender, EventArgs e)
         {
+            // Clear and Abandon the session
             Session.Clear();
             Session.Abandon();
+
+            // Redirect user to the login page
             Response.Redirect("user_login_page.aspx");
         }
     }
