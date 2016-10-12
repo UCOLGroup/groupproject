bd1def329f15dae91e6cd7270375b86f48acd28a
Dale Lochrie <0491707@uict.nz>
2016-10-13 11:02:18 +1300

    Commenting code

--- WebBased/WebBased/User_Login_Page.aspx.cs
+++ WebBased/WebBased/User_Login_Page.aspx.cs
@@ -11,9 +11,11 @@ namespace WebBased
 {
     public partial class User_Login_Page : System.Web.UI.Page
     {
+        //declare and initialize a variable called student
         public Student student = new Student();
-
+        //declare and initialize a database connection variable called connection
         private OleDbConnection connection = new OleDbConnection();
+        //declare and initialize a variable called UsernameLabel
         public static string UsernameLabel;
 
         protected void Page_Load(object sender, EventArgs e)
@@ -24,45 +26,43 @@ namespace WebBased
 
         protected void btnRegister_Click(object sender, EventArgs e)
         {
+            //point to the database file in the software directory for the Student table
             connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Student_Papers.accdb;
             Persist Security Info = False;";
+            //If the lecturer checkbox hasn't been checked run this code
             if (chkLecturerLogin.Checked == false)
             {
-                //Response.Write("Successful Registration");
+                //Open up a connection to the database
                 connection.Open();
-                //Creating a variable called command 
+                //declare and initialize a variable called command 
                 OleDbCommand command = new OleDbCommand();
+                //put the connection string into the command object
                 command.Connection = connection;
                 //Selecting the userinput from the login form and matching it with the database so that it can compare the username and password
                 command.CommandText = "Select * from students where user_name='" + tbxUsername.Text + "' and password='" + tbxPassword.Text + "';";
                 // Read out of the database
                 OleDbDataReader reader = command.ExecuteReader();
-                //count is Zero
+                //var count is set to Zero
                 int count = 0;
+                //Loop through records and returning them to Count if they match what the Student typed in.
                 while (reader.Read())
                 {
                     count = count + 1;
                 }
 
                 if (count == 1)
-                {   // Messagebox will show if the was login successful
-                    // MessageBox.Show("Login Successful");
+                {   
                     //Close the connection to the database
                     connection.Close();
-
+                    //Close the connection to the database
                     connection.Dispose();
-                    //this hides the first login form 
-                    // Students student = new Students();
-                    //student.User_name = tbxUsername.Text;
+                    //Updating the username label
                     UsernameLabel = tbxUsername.Text;
+                    //if the username and password are correct login to this session
                     Session["USER_ID"] = tbxUsername.Text;
+                    //open the lecturer page
                     Response.Redirect("Students_Course.aspx");
 
-
-
-                    //Student_Course SC = new Student_Course(student);
-                    //this shows the Student course form(i.e the second form)
-                    // SC.ShowDialog();
                 }
                 //if the username and password match another user in the database display this message
                 else if (count > 1)
@@ -73,13 +73,16 @@ namespace WebBased
                 //if the wrong password is entered Display this message "Wrong Username or Password"
                 else
                 {
+                    //alert message will show if the username and password don't match the Student table in the database.
                     Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('Wrong username & Password')</script>");
-                    // Response.Write("Wrong Username or Password");
+                    //clears the Username textbox
                     tbxUsername.Text = "";
+                    //clears the Password textbox
                     tbxPassword.Text = "";
+                    //return cursor focus onto the Username textbox 
                     tbxUsername.Focus();
                 }
-                // close the connection to the access database
+                // close the connection to the database
                 connection.Close();
             }
 
@@ -87,40 +90,52 @@ namespace WebBased
 
             if (chkLecturerLogin.Checked == true)
             {
+                //point to the database file in the software directory for the Lecturer table
                 connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Student_Papers.accdb;
             Persist Security Info = False;";
+                //Open up connection to the database
                 connection.Open();
+                //declare and initialize a variable called command
                 OleDbCommand command = new OleDbCommand();
+                //put the connection string into the command object
                 command.Connection = connection;
                 //Selecting the userinput from the login form and matching it with the database so that it can compare the username and password
                 command.CommandText = "Select * from lecturers where user_name='" + tbxUsername.Text + "' and password='" + tbxPassword.Text + "';";
                 // Read out of the database
                 OleDbDataReader reader = command.ExecuteReader();
+                //var count is set to Zero
                 int count = 0;
+                //Loop through records and returning them to Count if the Lecturer username and password match to the database.
                 while
                     (reader.Read())
                 {
                     count = count + 1;
                 }
-
+                //If the count has equals one 
                 if (count == 1)
                 {
+                    //close database connection
                     connection.Close();
-
+                    //close database connection
                     connection.Dispose();
+                    //if the username and password are correct login to this session
                     Session["USER_ID"] = tbxUsername.Text;
+                    //open the lecturer page
                     Response.Redirect("Lecturer.aspx");
                 }
 
                 else
                 {
+                    //alert message will show if the username and password don't match the lecturer table in the database.
                     Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('Wrong username & Password')</script>");
-                    // Response.Write("Wrong Username or Password");
+                    //clears the Username textbox
                     tbxUsername.Text = "";
+                    //clears the password textbox
                     tbxPassword.Text = "";
+                    //return cursor focus onto the Username textbox
                     tbxUsername.Focus();
                 }
-                // close the connection to the access database
+                // close the connection to the database
                 connection.Close();
 
             }
